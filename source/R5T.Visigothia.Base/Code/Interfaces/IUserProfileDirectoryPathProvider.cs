83541849ee40f4246ca9127e0c63c390078dd42f
David Coats <david@getsafetycone.com>
2021-05-17 22:19:56 -0700

    Add service interface definition type documentation.

--- source/R5T.Visigothia.Base/Code/Interfaces/IUserProfileDirectoryPathProvider.cs
+++ source/R5T.Visigothia.Base/Code/Interfaces/IUserProfileDirectoryPathProvider.cs
@@ -3,6 +3,10 @@
 
 namespace R5T.Visigothia
 {
+    /// <summary>
+    /// Provide the user directory of the user under which the current environment is running.
+    /// For examaple, on Windows, for user David, that directory would be C:\Users\David.
+    /// </summary>
     public interface IUserProfileDirectoryPathProvider
     {
         string GetUserProfileDirectoryPath();
