ffab531820320e4e21dfba2f45589eb0b8302958
davidcoats <david@getsafetycone.com>
2022-04-27 15:37:37 -0700

    Interim changes.

---
+++ source/R5T.Visigothia.Base/Code/Services/Definitions/IUserProfileDirectoryPathProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Visigothia
+{
+    /// <summary>
+    /// Provide the user directory of the user under which the current environment is running.
+    /// For examaple, on Windows, for user David, that directory would be C:\Users\David.
+    /// </summary>
+    [ServiceDefinitionMarker]
+    public interface IUserProfileDirectoryPathProvider : IServiceDefinition
+    {
+        string GetUserProfileDirectoryPath();
+    }
+}
